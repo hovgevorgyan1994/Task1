e6bf659c1715adfdf891274292089fcc31c8f823
Hovhannes Gevorgyan <hovosse888@gmail.com>
2022-10-12 23:27:26 +0400

    Initial commit

--- src/main/java/com/polixis/task1/controller/EmployeeController.java
+++ src/main/java/com/polixis/task1/controller/EmployeeController.java
@@ -2,14 +2,6 @@ package com.polixis.task1.controller;
 
 import com.polixis.task1.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
